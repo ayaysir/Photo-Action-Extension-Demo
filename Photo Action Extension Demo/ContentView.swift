2511a7dcfc5ff43c7a6f299f28008473edc8e19e
윤범태 <yoonbumtae@gmail.com>
2024-04-30 14:22:05 +0900

    Initial Commit

---
+++ Photo Action Extension Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Photo Action Extension Demo
+//
+//  Created by 윤범태 on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Photo Action Extension Demo/Photo_Action_Extension_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Action_Extension_DemoApp.swift
+//  Photo Action Extension Demo
+//
+//  Created by 윤범태 on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Action_Extension_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
